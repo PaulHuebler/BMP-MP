f9a69a67c81fcb4b42c8197713c5ca69e46817d1
PaulHuebler <paul.huebler@student.tu-freiberg.de>
2021-09-21 13:09:32 +0200

    grayscale

--- include/manipulations.h
+++ include/manipulations.h
@@ -1,8 +1,10 @@
 #ifndef MANIPULATIONS_H_
 #define MANIPULATIONS_H_
 
-void manipulate(char[], uint, int, int);
+void manipulate(char[], uint8_t, int, int);
 
 static void brightness(bitmap_pixel_hsv_t*, uint32_t, int);
+
+static void grayscale(bitmap_pixel_hsv_t*, uint32_t);
 
 #endif
--- src/manipulations.c
+++ src/manipulations.c
@@ -6,15 +6,33 @@
 #include "../include/bitmap.h"
 #include "../include/manipulations.h"
 
-void manipulate(char path[], uint manipulation, int opt1, int opt2) {
+void manipulate(char path[], uint8_t manipulation, int opt1, int opt2) {
+
+    bitmap_color_space_t colorspace;
+
+    //Die Manipulationen mit dem Index 1 bis 10 nutzen den RGB-Farbraum, die restlichen den HSV-Farbraum.
+    if (manipulation <= 10) {
+        colorspace = BITMAP_COLOR_SPACE_RGB;
+    } else {
+        colorspace = BITMAP_COLOR_SPACE_HSV;
+    }
 
     bitmap_pixel_hsv_t* pixels;
 	uint32_t widthPx, heightPx;
 
-	bitmap_error_t error = bitmapReadPixels(path, (bitmap_pixel_t**)&pixels, &widthPx, &heightPx, BITMAP_COLOR_SPACE_HSV);
+	bitmap_error_t error = bitmapReadPixels(path, (bitmap_pixel_t**)&pixels, &widthPx, &heightPx, colorspace);
 	assert(error == BITMAP_ERROR_SUCCESS);
 
-    brightness(pixels, widthPx * heightPx, 100);
+    switch(manipulation) {
+
+        case 11:    brightness(pixels, widthPx * heightPx, opt1);
+                    break;
+
+        case 12:    grayscale(pixels, widthPx * heightPx);
+                    break;
+
+        default:    printf("Die geforderte Manipulation steht noch nicht zur Verfügung.\n");
+    }
 
     bitmap_parameters_t params;
 
@@ -24,7 +42,7 @@ void manipulate(char path[], uint manipulation, int opt1, int opt2) {
 	params.colorDepth = BITMAP_COLOR_DEPTH_24;
 	params.compression = BITMAP_COMPRESSION_NONE;
 	params.dibHeaderFormat = BITMAP_DIB_HEADER_INFO;
-	params.colorSpace = BITMAP_COLOR_SPACE_HSV;
+	params.colorSpace = colorspace;
 
     error = bitmapWritePixels("../img/result.bmp", BITMAP_BOOL_TRUE, &params, (bitmap_pixel_t*)pixels);
 	assert(error == BITMAP_ERROR_SUCCESS);
@@ -50,5 +68,16 @@ static void brightness(bitmap_pixel_hsv_t* pixels, uint32_t count, int value)
 			else {
 				pix->v = (pix->v + value);
 			}
+		}	
+}
+
+static void grayscale(bitmap_pixel_hsv_t* pixels, uint32_t count)
+{
+		for (uint32_t x = 0; x < count; x++)
+		{
+
+			bitmap_pixel_hsv_t* pix = &pixels[x];
+
+			pix->s = 0;
 		}	
 }
