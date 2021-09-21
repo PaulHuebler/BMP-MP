05646fd19fe3f2398956da16186ea4944d808cdf
bennib311 <benjamin.backhaus311@gmail.com>
2021-09-21 16:40:01 +0200

    update

--- src/image.c
+++ src/image.c
@@ -1,6 +1,8 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 
- 
+
+
 static void menu_response(GtkWidget* menu_item, gpointer data)
 {
     if(strcmp(gtk_menu_item_get_label(GTK_MENU_ITEM(menu_item)), "New") == 0)   
@@ -9,12 +11,40 @@ static void menu_response(GtkWidget* menu_item, gpointer data)
     }
     if(strcmp(gtk_menu_item_get_label(GTK_MENU_ITEM(menu_item)), "Exit") == 0)  
     {
-        g_print("You pressed Exit\n");
+        gtk_main_quit(); //quit the application
     }
     if(strcmp(gtk_menu_item_get_label(GTK_MENU_ITEM(menu_item)), "About") == 0)
     {
         g_print("You pressed About\n");
     }
+}
+
+GtkWidget *
+create_filechooser_dialog(char *init_path, GtkFileChooserAction action)           // https://stackoverflow.com/questions/30751178/gtk3-file-chooser-in-a-non-gtk-application
+{
+  GtkWidget *wdg = NULL;
+
+  switch (action) {
+    case GTK_FILE_CHOOSER_ACTION_SAVE:
+      wdg = gtk_file_chooser_dialog_new("Save file", NULL, action,
+        "Cancel", GTK_RESPONSE_CANCEL,
+        "Save", GTK_RESPONSE_OK,
+        NULL);
+      break;
+
+    case GTK_FILE_CHOOSER_ACTION_OPEN:
+      wdg = gtk_file_chooser_dialog_new("Open file", NULL, action,
+        "Cancel", GTK_RESPONSE_CANCEL,
+        "Open", GTK_RESPONSE_OK,
+        NULL);
+      break;
+
+    case GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER:
+    case GTK_FILE_CHOOSER_ACTION_CREATE_FOLDER:
+      break;
+  }
+
+  return wdg;
 }
 
 GdkPixbuf *create_pixbuf(const gchar * filename) {
@@ -40,6 +70,8 @@ int main (int    argc, char **argv)
   GtkWidget *layout;
   GtkWidget *image;
   GtkWidget *menu_bar, *menu_item, *file_menu, *help_menu, *vbox, *button;
+  GtkWidget *wdg;
+  char *file_name = "";
 
   gtk_init(&argc, &argv);
 
@@ -83,15 +115,18 @@ int main (int    argc, char **argv)
   g_signal_connect(menu_item, "activate", G_CALLBACK(menu_response), NULL);
 
   vbox = gtk_box_new(0,0);
-  button = gtk_button_new_with_label("This is a Button");
+  //button = gtk_button_new_with_label("This is a Button");
   gtk_box_pack_start(GTK_BOX(vbox), menu_bar,1,0,0);
-  gtk_box_pack_start(GTK_BOX(vbox), button,0,0,0);
+  //gtk_box_pack_start(GTK_BOX(vbox), button,0,0,0);
  
   gtk_layout_put(GTK_LAYOUT(layout), vbox, 0, 0);
 
+// open a file
+  wdg = create_filechooser_dialog(file_name, GTK_FILE_CHOOSER_ACTION_OPEN);
+  
 // show image
-  image = gtk_image_new_from_file ("../img/test.bmp");
-  gtk_layout_put(GTK_LAYOUT(layout), image, 0, 80);
+  image = gtk_image_new_from_file (file_name);
+  gtk_layout_put(GTK_LAYOUT(layout), image, 20, 80);
 
   gtk_widget_show_all(window);
 
